01507c89d64c2e5c1c3196580c4c7391c4aba2df
Kyle Gorkowski <kkgorkow@gmail.com>
2025-06-20 14:22:34 -0600

    adde copilot:

--- init.lua
+++ init.lua
@@ -377,6 +377,21 @@ require('lazy').setup({
     },
   },
 
+  --copilot
+  {
+    'github/copilot.vim',
+    event = 'InsertEnter',
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap('i', '<C-J>', 'copilot#Accept("<CR>")', {
+        expr = true,
+        silent = true,
+        script = true,
+        noremap = true,
+      })
+    end,
+  },
+
   -- zen-mode
   {
     'folke/zen-mode.nvim',
