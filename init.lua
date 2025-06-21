c3b871077ad4bf6a035a6debd42de4594d7fdcae
Kyle Gorkowski <kkgorkow@gmail.com>
2025-06-20 19:59:39 -0600

    added auto save

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 --[[
+--
 
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
@@ -378,6 +379,42 @@ require('lazy').setup({
       { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
       { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
     },
+  },
+
+  -- auto-save files
+  {
+    'Pocco81/auto-save.nvim',
+    event = { 'InsertLeave', 'TextChanged' },
+    config = function()
+      require('auto-save').setup {
+        enabled = true,
+        execution_message = {
+          message = function()
+            return ('AutoSave: saved at ' .. vim.fn.strftime '%H:%M:%S')
+          end,
+          dim = 0.18,
+          cleaning_interval = 1250,
+        },
+        trigger_events = { 'InsertLeave', 'TextChanged' },
+        condition = function(buf)
+          local fn = vim.fn
+          local utils = require 'auto-save.utils.data'
+          if fn.getbufvar(buf, '&modifiable') == 1 and utils.not_in(fn.getbufvar(buf, '&filetype'), {}) then
+            return true
+          end
+          return false
+        end,
+        write_all_buffers = false,
+        debounce_delay = 135,
+        callbacks = {
+          enabling = nil,
+          disabling = nil,
+          before_asserting_save = nil,
+          before_saving = nil,
+          after_saving = nil,
+        },
+      }
+    end,
   },
 
   --copilot
